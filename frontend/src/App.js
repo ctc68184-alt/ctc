5251bd76c68105c07182988a748a126ce62e9967
romnick <romnicksanjuan9@gmail.com>
2026-03-01 11:52:16 +0800

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,31 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 
-import Dashboard from './components/Dashboard';
-import BorrowedItems from './components/BorrowedItems';
-import AddBorrowItem from './components/AddBorrowItem';
-import CreateProduct from './components/merchandise/CreateProduct';
-import Merchandise from './components/merchandise/Merchandise.js';
-import PurchaseHistory from './components/merchandise/PurchaseHistory.js';
-import Login from './components/Login.js';
-import ForgotPassword from './components/ForgotPassword.js';
+// import Dashboard from './components/Dashboard';
+// import BorrowedItems from './components/BorrowedItems';
+// import AddBorrowItem from './components/AddBorrowItem';
+// import CreateProduct from './components/merchandise/CreateProduct';
+// import Merchandise from './components/merchandise/Merchandise.js';
+// import PurchaseHistory from './components/merchandise/PurchaseHistory.js';
+// import Login from './components/Login.js';
+// import ForgotPassword from './components/ForgotPassword.js';
 // import Test from './components/Test.js';
-import BorrowedTransaction from './components/BorrowedTransaction.js';
-import MerchandiseTransaction from './components/merchandise/MerchandiseTransaction.js';
-import SignUp from './components/SignUp.js';
-import BorrowForm from './components/BorrowForm.js';
-import ManageMerchandise from './components/merchandise/ManageMerchandise.js';
-import Inventory from './components/Inventory.js';
-import ReturnedItems from './components/ReturnedItems.js';
-import Settings from './components/Settings.js';
-import RequestItems from './components/RequestItems.js';
-import Users from './components/Users.js';
-import Reports from './components/Reports.js';
-import RequestFullDetails from './components/RequestFullDetails.js';
-import Home from './components/home/Home.js';
-import RequesterSignUp from './components/requesterUser/RequesterSignUp.js';
-import RequesterSignIn from './components/requesterUser/RequesterSignIn.js';
-import BorrowedItemFullDetails from './components/BorrowedItemFullDetails.js';
+// import BorrowedTransaction from './components/BorrowedTransaction.js';
+// import MerchandiseTransaction from './components/merchandise/MerchandiseTransaction.js';
+// import SignUp from './components/SignUp.js';
+// import BorrowForm from './components/BorrowForm.js';
+// import ManageMerchandise from './components/merchandise/ManageMerchandise.js';
+// import Inventory from './components/Inventory.js';
+// import ReturnedItems from './components/ReturnedItems.js';
+// import Settings from './components/Settings.js';
+// import RequestItems from './components/RequestItems.js';
+// import Users from './components/Users.js';
+// import Reports from './components/Reports.js';
+// import RequestFullDetails from './components/RequestFullDetails.js';
+// import Home from './components/home/Home.js';
+// import RequesterSignUp from './components/requesterUser/RequesterSignUp.js';
+// import RequesterSignIn from './components/requesterUser/RequesterSignIn.js';
+// import BorrowedItemFullDetails from './components/BorrowedItemFullDetails.js';
 
 // import Test from './components/Category.js';
 // import Testing from './components/Test.js';
@@ -36,10 +36,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        {/* <Route path='/' element={<Home />} />
         <Route path='/admin-login' element={<Login />} />
 
-        {/* requester */}
+        requester
         <Route path='/requester-sign-up' element={<RequesterSignUp />} />
         <Route path='/requester-login' element={<RequesterSignIn />} />
 
@@ -64,7 +64,7 @@ function App() {
         <Route path='/reports' element={<Reports />} />
         <Route path='/request-full-details' element={<RequestFullDetails />} />
 
-        {/* <Route path='/test' element={<Test />} /> */}
+        <Route path='/test' element={<Test />} /> */}
       </Routes>
     </BrowserRouter>
   );
